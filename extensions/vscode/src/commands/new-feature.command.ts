782bb513623e29e6872ab2433984460567963221
David <davidkisbeygreen145@gmail.com>
2023-10-08 07:36:30 +0100

    extension

--- extensions/vscode/src/commands/new-feature.command.ts
+++ extensions/vscode/src/commands/new-feature.command.ts
@@ -1,4 +1,4 @@
-import { Uri, window } from "vscode";
+import { Uri, window, workspace } from "vscode";
 import { exec } from "child_process";
 import { getTargetDirectory } from "../utils/get-target-directory";
 
@@ -12,12 +12,26 @@ export const newFeature = async (args: Uri) => {
 
     if (name) {
       const commandNewFeature = `rn add feature --name ${name} --path ${targetDir}`;
-      exec(commandNewFeature, (error, stdout, stderr) => {
+      exec(commandNewFeature, async (error, stdout, stderr) => {
         if (error) {
           // Handle the error by displaying an error message to the user
           window.showErrorMessage(`Error: ${error.message}`);
         } else {
           // Process the command's output (if needed)
+
+          // Run the build runner command
+          const buildRunnerCommand = "flutter pub run build_runner build --delete-conflicting-outputs";
+          const workspaceFolder = workspace.workspaceFolders?.[0];
+          if (workspaceFolder) {
+            const buildRunnerResult = await runCommandInWorkspaceFolder(workspaceFolder.uri.fsPath, buildRunnerCommand);
+            if (buildRunnerResult.error) {
+              window.showErrorMessage(`Error running build runner: ${buildRunnerResult.error}`);
+            } else {
+              window.showInformationMessage("Feature created successfully and build runner completed.");
+            }
+          } else {
+            window.showWarningMessage("No workspace folder found.");
+          }
         }
       });
     }
@@ -25,4 +39,17 @@ export const newFeature = async (args: Uri) => {
     // Handle exceptions (e.g., if showInputBox or getTargetDirectory fails)
     window.showErrorMessage(`Error: ${error}`);
   }
+};
+
+// Function to run a command in a specified workspace folder
+const runCommandInWorkspaceFolder = async (folderPath: string, command: string): Promise<{ error?: string }> => {
+  return new Promise((resolve) => {
+    exec(command, { cwd: folderPath }, (error, stdout, stderr) => {
+      if (error) {
+        resolve({ error: error.message });
+      } else {
+        resolve({});
+      }
+    });
+  });
 };
