75d0305b376aaf090b2cc789c74fece8766a4aac
David <davidkisbeygreen145@gmail.com>
2023-10-08 05:40:38 +0100

    extension clean

--- extensions/vscode/src/extension.ts
+++ extensions/vscode/src/extension.ts
@@ -1,16 +1,11 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as cp from "child_process";
-import * as path from "path";
+
 import { newFeature } from "./commands/new-feature.command";
 import { addComponent } from "./commands/add-component.command";
-import { addPage } from "./commands/add-page.command";
 import { commands, ExtensionContext } from "vscode";
-import { addBloc } from "./commands/add-bloc.command";
-import { addCubit } from "./commands/add-cubit.command";
 import { addModel } from "./commands/add-model.command";
 import { addFile } from "./commands/add-file.command";
-import { addBlocEvent } from "./commands/add-bloc-event.command";
 import { addFormInput } from "./commands/add-input.command";
 
 // this method is called when your extension is activated
