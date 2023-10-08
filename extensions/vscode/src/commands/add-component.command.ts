3313529b36669da7aa5a9fc856083d7b3fed2ebe
David <davidkisbeygreen145@gmail.com>
2023-10-08 11:29:30 +0100

    components adjustment

--- extensions/vscode/src/commands/add-component.command.ts
+++ extensions/vscode/src/commands/add-component.command.ts
@@ -1,21 +1,26 @@
 import { exec } from "child_process";
-import { Uri, window,workspace } from "vscode";
+import { Uri, window } from "vscode";
 import { getTargetDirectory } from "../utils/get-target-directory";
 import { buildRunner } from "../utils/build_runner";
-
 
 export const addComponent = async (args: Uri) => {
   const name = await window.showInputBox({
     prompt: "Name of the component",
     placeHolder: "Component name",
   });
-  const targetDir = await getTargetDirectory(args);
+  var targetDir = await getTargetDirectory(args);
 
-  if (name) {
+  if (name&&targetDir) {
+    // Check if the targetDir ends with "components"
+    if (!targetDir.endsWith("components")) {
+      targetDir += "/components";
+    }
+
     let child = exec(
-      `render add component --name ${name} --path ${targetDir}/components`
+      `render add component --name ${name} --path ${targetDir}`
     );
     child.stderr?.on("data", (data) => window.showErrorMessage(data));
-   await buildRunner("Component");
+    
+    await buildRunner("Component");
   }
 };
