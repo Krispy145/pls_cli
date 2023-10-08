ee0015709273e4185f0b88f0d25f7ebeb27ef86e
David <davidkisbeygreen145@gmail.com>
2023-10-08 08:19:31 +0100

    Added extre logger extension methods

--- extensions/vscode/src/commands/new-feature.command.ts
+++ extensions/vscode/src/commands/new-feature.command.ts
@@ -1,6 +1,7 @@
 import { Uri, window, workspace } from "vscode";
 import { exec } from "child_process";
 import { getTargetDirectory } from "../utils/get-target-directory";
+import * as path from "path"; // Add the path library
 
 export const newFeature = async (args: Uri) => {
   try {
@@ -11,40 +12,61 @@ export const newFeature = async (args: Uri) => {
     const targetDir = await getTargetDirectory(args);
 
     if (name) {
-      const commandNewFeature = `rn add feature --name ${name} --path ${targetDir}`;
-      exec(commandNewFeature, async (error, stdout, stderr) => {
-        if (error) {
-          // Handle the error by displaying an error message to the user
-          window.showErrorMessage(`Error creating feature: ${error.message}`);
+      // Create a logger feature string using the provided name
+      const loggerFeatureString = createLoggerFeatureString(name);
+
+      // Get the path to the logger feature file
+      const loggerFeatureFilePath = getLoggerFeatureFilePath();
+
+      // Write the logger feature string to the file
+      writeFile(loggerFeatureFilePath, loggerFeatureString);
+
+      // Continue with running the build runner command
+      const buildRunnerCommand = "flutter pub run build_runner build --delete-conflicting-outputs";
+      const workspaceFolder = workspace.workspaceFolders?.[0];
+      if (workspaceFolder) {
+        const buildRunnerResult = await runCommandInWorkspaceFolder(workspaceFolder.uri.fsPath, buildRunnerCommand);
+        if (buildRunnerResult.error) {
+          window.showErrorMessage(`Error running build runner: ${buildRunnerResult.error}`);
         } else {
-          // Process the command's output (if needed)
-          const commandNewLoggerFeature = `rn add logger --name ${name}`;
-          exec(commandNewLoggerFeature, async (loggerError, loggerStdout, loggerStderr) => {
-            if (loggerError) {
-              // Handle the error by displaying an error message to the user
-              window.showErrorMessage(`Error creating logger feature: ${loggerError.message}`);
-            } else {
-              // Run the build runner command
-              const buildRunnerCommand = "flutter pub run build_runner build --delete-conflicting-outputs";
-              const workspaceFolder = workspace.workspaceFolders?.[0];
-              if (workspaceFolder) {
-                const buildRunnerResult = await runCommandInWorkspaceFolder(workspaceFolder.uri.fsPath, buildRunnerCommand);
-                if (buildRunnerResult.error) {
-                  window.showErrorMessage(`Error running build runner: ${buildRunnerResult.error}`);
-                } else {
-                  window.showInformationMessage("Feature created successfully and build runner completed.");
-                }
-              } else {
-                window.showWarningMessage("No workspace folder found.");
-              }
-            }
-          });
+          window.showInformationMessage("Feature created successfully and build runner completed.");
         }
-      });
+      } else {
+        window.showWarningMessage("No workspace folder found.");
+      }
     }
   } catch (error) {
     // Handle exceptions (e.g., if showInputBox or getTargetDirectory fails)
     window.showErrorMessage(`Error: ${error}`);
+  }
+};
+
+// Function to create the logger feature string
+const createLoggerFeatureString = (name: string): string => {
+  // Manually implement camelCase and constantCase
+  const camelCaseName = toCamelCase(name);
+  const constantCaseName = toConstantCase(name);
+
+  return `static final LoggerFeature ${camelCaseName} = LoggerFeature("${constantCaseName}", true);\n///LOGGER FEATURE END`;
+};
+
+// Function to convert a string to camelCase
+const toCamelCase = (str: string): string => {
+  return str.replace(/[-_]+(.)?/g, (_, c) => (c ? c.toUpperCase() : ""));
+};
+
+// Function to convert a string to CONSTANT_CASE
+const toConstantCase = (str: string): string => {
+  return str.replace(/[-\s]+/g, "_").toUpperCase();
+};
+
+// Function to get the path to the logger feature file
+const getLoggerFeatureFilePath = (): string => {
+  const currentWorkspace = workspace.workspaceFolders?.[0];
+  if (currentWorkspace) {
+    return path.join(currentWorkspace.uri.fsPath, "lib", "utils", "logger_features.dart");
+  } else {
+    throw new Error("No workspace folder found.");
   }
 };
 
@@ -59,4 +81,14 @@ const runCommandInWorkspaceFolder = async (folderPath: string, command: string):
       }
     });
   });
+};
+
+// Function to write content to a file (requires the 'fs' library)
+const writeFile = (filePath: string, content: string) => {
+  try {
+    const fs = require("fs");
+    fs.writeFileSync(filePath, content, "utf-8");
+  } catch (error) {
+    throw new Error(`Error writing to file: ${error}`);
+  }
 };
