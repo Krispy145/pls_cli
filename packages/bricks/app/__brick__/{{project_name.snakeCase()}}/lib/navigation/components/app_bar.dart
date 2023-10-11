acd279cc03a243cdaafcb33468536ffa3b4cf3f3
David <davidkisbeygreen145@gmail.com>
2023-10-11 20:29:02 +0100

    Update: app bundle

--- packages/bricks/app/__brick__/{{project_name.snakeCase()}}/lib/navigation/components/app_bar.dart
+++ packages/bricks/app/__brick__/{{project_name.snakeCase()}}/lib/navigation/components/app_bar.dart
@@ -3,12 +3,10 @@ import 'package:flutter/material.dart';
 /// [MainAppBar] is a class that defines the main app bar of the app.
 class MainAppBar extends StatelessWidget implements PreferredSizeWidget {
   /// [MainAppBar] constructor.
-  const MainAppBar({
-    super.key,
-  }) : preferredSize = const Size.fromHeight(kToolbarHeight);
+  const MainAppBar({super.key});
 
   @override
-  final Size preferredSize;
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
 
   @override
   Widget build(BuildContext context) {
