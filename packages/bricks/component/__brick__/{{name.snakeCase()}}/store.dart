41ac7929524e546ff7465114a573130567337eab
David <davidkisbeygreen145@gmail.com>
2023-10-11 20:27:18 +0100

    Update: updated components bundle

--- packages/bricks/component/__brick__/{{name.snakeCase()}}/store.dart
+++ packages/bricks/component/__brick__/{{name.snakeCase()}}/store.dart
@@ -1,17 +1,12 @@
 import 'package:mobx/mobx.dart';
+import 'package:utilities/widgets/load_state/base_store.dart';
 
 part 'store.g.dart';
 
+/// [{{name.pascalCase()}}Store] is a class that uses [{{name.pascalCase()}}StoreBase] for the {{name.pascalCase()}} component.
 class {{name.pascalCase()}}Store = {{name.pascalCase()}}StoreBase with _${{name.pascalCase()}}Store;
 
-abstract class {{name.pascalCase()}}StoreBase with Store {
-  @observable
-  bool _isBusy = false;
-
-  @computed
-  bool get isBusy => _isBusy;
-
-  set isBusy(bool value) {
-    _isBusy = value;
-  }
+/// [{{name.pascalCase()}}StoreBase] is a class that defines the base store of the {{name.pascalCase()}} component.
+abstract class {{name.pascalCase()}}StoreBase extends LoadStateStore with Store {
+  /// {{name.pascalCase()}} component logic goes here.
 }
--- packages/bricks/component/__brick__/{{name.snakeCase()}}/widget.dart
+++ packages/bricks/component/__brick__/{{name.snakeCase()}}/widget.dart
@@ -1,13 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:utilities/widgets/load_state/state_widget.dart';
 import 'store.dart';
 
-
+/// [{{name.pascalCase()}}] is a component class.
 class {{name.pascalCase()}} extends StatelessWidget {
-   {{name.pascalCase()}}({super.key});
+  /// [{{name.pascalCase()}}] constructor.
+  {{name.pascalCase()}}({super.key});
 
-final {{name.pascalCase()}}Store {{name.camelCase()}}Store = {{name.pascalCase()}}Store();
+  /// [{{name.camelCase()}}Store] is a [{{name.pascalCase()}}Store] instance.
+  final {{name.pascalCase()}}Store {{name.camelCase()}}Store = {{name.pascalCase()}}Store();
   @override
   Widget build(BuildContext context) {
-    return const SizedBox();
+    return LoadStateBuilder(
+      viewStore: {{name.camelCase()}}Store,
+      loadedBuilder: (context) => const Center(
+        child: Text("Loaded"),
+      ),
+      emptyBuilder: (context) => const Center(
+        child: Text("Empty"),
+      ),
+      errorBuilder: (context) => const Center(
+        child: Text("Error"),
+      ),
+    );
   }
 }
