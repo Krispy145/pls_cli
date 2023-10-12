e258716b4d692b2fe71d4a6a0e86228f7516d655
David <davidkisbeygreen145@gmail.com>
2023-10-12 10:20:52 +0100

    Update script path

--- extensions/vscode/src/commands/add-admob.command.ts
+++ extensions/vscode/src/commands/add-admob.command.ts
@@ -8,7 +8,7 @@ import * as fs from "fs";
 import * as path from "path";
 
 export const addAdmob = async (args: Uri) => {
-  const pythonScriptName = './out/scripts/admob.py'; 
+  const pythonScriptName = 'scripts/admob.py'; 
   const extensionPath = __dirname;
 
   // Create a terminal instance
