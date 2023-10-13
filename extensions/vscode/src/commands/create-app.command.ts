5e0b1ff45e84bc32f589da7b9631da7b57cde598
David <davidkisbeygreen145@gmail.com>
2023-10-13 21:11:01 +0100

    Create App: new extension

--- extensions/vscode/src/commands/create-app.command.ts
+++ extensions/vscode/src/commands/create-app.command.ts
@@ -1,8 +1,8 @@
-import * as vscode from 'vscode';
-import * as path from 'path';
 
-export async function createApp() {
-    const folderUri = await vscode.window.showOpenDialog({
+import { Uri, window, workspace } from "vscode";
+
+export const createApp = async (args: Uri) => {
+    const folderUri = await window.showOpenDialog({
         canSelectFiles: false,
         canSelectFolders: true,
         canSelectMany: false,
@@ -10,27 +10,27 @@ export async function createApp() {
     });
 
     if (!folderUri || folderUri.length === 0) {
-        vscode.window.showErrorMessage('No directory selected.');
+        window.showErrorMessage('No directory selected.');
         return;
     }
 
     const appDirectory = folderUri[0].fsPath;
 
-    const appName = await vscode.window.showInputBox({
+    const appName = await window.showInputBox({
         prompt: 'Enter the name for your new app',
         placeHolder: 'digital_oasis_app',
     });
 
     if (!appName) {
-        vscode.window.showErrorMessage('App name not provided. Aborting.');
+        window.showErrorMessage('App name not provided. Aborting.');
         return;
     }
 
-    const terminal = vscode.window.createTerminal('Create New App');
+    const terminal = window.createTerminal('Create New App');
     const cmd = `rn create app ${appName}`;
 
     // Change to the appDirectory and run the command
     terminal.sendText(`cd "${appDirectory}"`);
     terminal.sendText(cmd);
     terminal.show();    
-}
+};
