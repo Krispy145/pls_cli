3e02a42eb0579f055e569de67e3851aff1f0fc0c
David <davidkisbeygreen145@gmail.com>
2023-10-30 06:02:38 +0000

    Add: updated extension (ongoing)

--- extensions/vscode/src/extension.ts
+++ extensions/vscode/src/extension.ts
@@ -6,15 +6,13 @@ import { addComponent } from "./commands/add-component.command";
 import { commands, ExtensionContext } from "vscode";
 import { addModel } from "./commands/add-model.command";
 import { addAdmob } from "./commands/add-admob.command";
+import { addNotifications } from "./commands/add-notifications.command";
 import { createApp } from "./commands/create-app.command";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: ExtensionContext) {
-  let app = commands.registerCommand(
-    "digital-oasis.create-app",
-    createApp
-  );
+  let app = commands.registerCommand("digital-oasis.create-app", createApp);
   let feature = commands.registerCommand(
     "digital-oasis.new-feature",
     newFeature
@@ -25,9 +23,21 @@ export function activate(context: ExtensionContext) {
   );
   let admob = commands.registerCommand("digital-oasis.add-admob", addAdmob);
 
+  let notifications = commands.registerCommand(
+    "digital-oasis.add-notifications",
+    addNotifications
+  );
+
   let model = commands.registerCommand("digital-oasis.add-model", addModel);
 
-  context.subscriptions.push(app,feature, component, model, admob);
+  context.subscriptions.push(
+    app,
+    feature,
+    component,
+    model,
+    admob,
+    notifications
+  );
 }
 
 // this method is called when your extension is deactivated
