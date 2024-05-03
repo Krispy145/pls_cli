493a159380464fd258cd1053ec53fc355c6bb202
David <davidkisbeygreen145@gmail.com>
2024-05-03 11:58:31 +0400

    feat(snippets): added dart snippets json to vscode extension

--- packages/bricks/model/__brick__/{{name.snakeCase()}}_model.dart
+++ packages/bricks/model/__brick__/{{name.snakeCase()}}_model.dart
@@ -3,34 +3,34 @@ import "package:dart_mappable/dart_mappable.dart";
 part "{{name.snakeCase()}}_model.mapper.dart";
 
 @MappableClass(caseStyle: CaseStyle.snakeCase)
-class {{name.pascalCase()}}Model with {{name.pascalCase()}}ModelMappable {
+class $0Model with $0ModelMappable {
   final String id;
   final String name;
 
-  {{name.pascalCase()}}Model({
+  $0Model({
     required this.id,
     required this.name,
   });
 
-  static const fromMap = {{name.pascalCase()}}ModelMapper.fromMap;
-  static const fromJson = {{name.pascalCase()}}ModelMapper.fromJson;
+  static const fromMap = $0ModelMapper.fromMap;
+  static const fromJson = $0ModelMapper.fromJson;
 
-  static final {{name.camelCase()}}One = {{name.pascalCase()}}Model(
+  static final {{name.camelCase()}}One = $0Model(
     id: "{{name.camelCase()}}OneId",
     name: "{{name.titleCase()}} One",    
   );
   
-  static final {{name.camelCase()}}Two = {{name.pascalCase()}}Model(
+  static final {{name.camelCase()}}Two = $0Model(
     id: "{{name.camelCase()}}TwoId",
     name: "{{name.titleCase()}} Two",    
   );
   
-  static final {{name.camelCase()}}Three = {{name.pascalCase()}}Model(
+  static final {{name.camelCase()}}Three = $0Model(
     id: "{{name.camelCase()}}ThreeId",
     name: "{{name.titleCase()}} Three",    
   );
 
-  static final List<{{name.pascalCase()}}Model> fakeData = [
+  static final List<$0Model> fakeData = [
     {{name.camelCase()}}One,
     {{name.camelCase()}}Two,
     {{name.camelCase()}}Three,
